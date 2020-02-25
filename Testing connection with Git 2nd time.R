dbf939b96dbb5d03ffa41b0f90612f43f6484a5f
Dave Wegley <dwegley@mnu.edu>
2020-02-25 16:49:31 -0600

    This is my 2nd time with this step.  I did this Monday afternoon on Feb 25.

---
+++ Testing connection with Git 2nd time.R
@@ -0,0 +1,3 @@
+print("This file was created within RStudio") 
+print("This is the second time of done this step. I'm redoing a few steps to make sure I'm doing them right") 
+print("And now it lives on GitHub")
